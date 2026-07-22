2c04b09bf36d8923fbbd018086051184e53a8769
unknown <imhyrongyun365@gmail.com>
2026-07-22 09:40:31 +0900

    전자결재 페이지

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,8 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactStrictMode: true,
-  devIndicators: false,
+  // reactStrictMode: true,
+  // devIndicators: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*", // 백엔드 포트에 맞게 수정
+      },
+    ];
+  },
 };
 
 export default nextConfig;
