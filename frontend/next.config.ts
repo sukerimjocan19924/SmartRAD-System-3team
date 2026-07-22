2fa53f16d6464ed92b0dc6e899966b11f09bce05
seokhyeon09 <ring4392@gmail.com>
2026-07-22 17:19:45 +0900

    Fix: nextjs proxy config for docker

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
   // reactStrictMode: true,
   // devIndicators: false,
   async rewrites() {
-    const backendUrl = process.env.BACKEND_URL || "http://localhost:8080";
+    const backendUrl = process.env.BACKEND_URL || "http://127.0.0.1:8080";
     return [
       {
         source: "/api/:path*",
